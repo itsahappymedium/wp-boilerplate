5223d1858674a429047f1e5d30d5023e63317659
Kodie Grantham <kodie.grantham@gmail.com>
2022-03-24 11:59:51 -0500

    Fix ACF JSON syncing

--- content/themes/theme/lib/acf.php
+++ content/themes/theme/lib/acf.php
@@ -1,8 +1,14 @@
 <?php
-// Sync ACF field groups to JSON files so they can be version-controlled
-add_filter('acf/settings/save_json', 'hm_acf_json_version_control');
-add_filter('acf/settings/load_json', 'hm_acf_json_version_control');
-function hm_acf_json_version_control($paths) {
+// Set the path where ACF JSON data should be saved to
+add_filter('acf/settings/save_json', 'hm_acf_json_save');
+function hm_acf_json_save($path) {
+  $path = WP_CONTENT_DIR . '/acf-json';
+  return $path;
+}
+
+// Set the path where ACF JSON data should be loaded from
+add_filter('acf/settings/load_json', 'hm_acf_json_load');
+function hm_acf_json_load($paths) {
   unset($paths[0]);
   $paths[] = WP_CONTENT_DIR . '/acf-json';
   return $paths;
