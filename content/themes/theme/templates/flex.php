d17924849d28c139e6e8afa8049378b87378540e
Kodie Grantham <kodie.grantham@gmail.com>
2022-10-24 13:11:15 -0500

    Prepend the flex variables names too just to be safe

--- content/themes/theme/templates/flex.php
+++ content/themes/theme/templates/flex.php
@@ -1,9 +1,9 @@
 <?php
-$field_name = arrval($args, 'flex_field_name', 'flex_sections');
-$post_id = arrval($args, 'flex_post_id', false);
+$flex_field_name = arrval($args, 'flex_field_name', 'flex_sections');
+$flex_post_id = arrval($args, 'flex_post_id', false);
 
-if (function_exists('have_rows') && have_rows($field_name, $post_id)) {
-  while (have_rows($field_name, $post_id)) {
+if (function_exists('have_rows') && have_rows($flex_field_name, $flex_post_id)) {
+  while (have_rows($flex_field_name, $flex_post_id)) {
     the_row();
     get_template_part('templates/' . get_row_layout(), null, $args);
   }
