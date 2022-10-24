146d8a8fa873b116549debd359d7241ad0ede425
Kodie Grantham <kodie.grantham@gmail.com>
2022-10-24 12:57:22 -0500

    Allow flex template to have it's field name and post_id set

--- content/themes/theme/templates/flex.php
+++ content/themes/theme/templates/flex.php
@@ -1,6 +1,9 @@
 <?php
-if (function_exists('have_rows') && have_rows('flex_sections')) {
-  while (have_rows('flex_sections')) {
+$field_name = arrval($args, 'field_name', 'flex_sections');
+$post_id = arrval($args, 'post_id', false);
+
+if (function_exists('have_rows') && have_rows($field_name, $post_id)) {
+  while (have_rows($field_name, $post_id)) {
     the_row();
     get_template_part('templates/' . get_row_layout(), null, $args);
   }
